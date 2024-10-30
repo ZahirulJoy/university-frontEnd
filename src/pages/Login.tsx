404c52e4fd0298ee456a8a12dc0ebfa5bbfffe38
ZahirulIslamJoy <neloy1613@gmail.com>
2024-10-30 12:17:31 +0600

    completed making resuasble form with and design and react-hook-form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,49 +1,46 @@
-import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
-import { useLoginMutation } from '../redux/features/auth/authApi';
-import { useAppDispatch } from '../redux/hook';
-import { setUser } from '../redux/features/auth/authSlice';
-import { verifyToken } from '../utils/verifyToken';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'sonner';
+import { Button, Row } from "antd";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
 
 const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      userId: 'A-0001',
-      password: 'admin123',
-    },
-  });
 
   const [login] = useLoginMutation();
 
-  const onSubmit = async (data: { userId: string; password: string; }) => {
-    const userInfo = {
-      id: data.userId,
-      password: data.password,
-    };
+  const defaultValues = {
+    userId: 'A-0002',
+    password: 'admin123',
+  };
 
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
-    dispatch(setUser({ user: user, token: res.data.accessToken }));
-    toast.success("Login Is Successfull")
-    navigate(`/${user.role}/dashboard`)
+  const onSubmit = async (data: { userId: string; password: string }) => {
+    console.log(data);
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
+
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("Login Is Successfull")
+      navigate(`/${user.role}/dashboard`)
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('userId')} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
-      </div>
+    <Row justify={"center"} align={"middle"} style={{height:"100vh"}}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues} >
+      <PHInput type="text" name="userId" label="Id"></PHInput>
+      <PHInput type="text" name="password" label="password"></PHInput>
       <Button htmlType="submit">Login</Button>
-    </form>
+    </PHForm>
+    </Row>
   );
 };
 
